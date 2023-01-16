18ba0d0752b00181b2e540d7ccddbf961b28a1d2
ogzambrano <ogzambranop@gmail.com>
2023-01-16 19:13:29 -0400

    b3

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @Value("${app.message}")
     String message;
-    String hola;
+    String hola="hola";
 
     @GetMapping("/hola")
     public String holaMundo(){ return "hola mundo, how are you? "+message;}
